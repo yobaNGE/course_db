c4a3c030af54a3bdfb0c99208a6b77d963c58f88
Suomi228 <bixep345@gmail.com>
2024-05-25 20:13:26 +0300

    переход

--- src/main/java/org/example/course/HelloController.java
+++ src/main/java/org/example/course/HelloController.java
@@ -1,12 +1,24 @@
 package org.example.course;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import org.example.course.entities.Provider;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button go;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -22,5 +34,15 @@ public class HelloController {
             //welcomeText.setText("Welcome to JavaFX Application!");
         });
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    void goProvider(ActionEvent event) throws IOException { // GO BACK!!!
+        Stage stage = (Stage) go.getScene().getWindow();
+        Parent root =
+                FXMLLoader.load(getClass().getResource("provider.fxml"));
+                ;
+        stage.setTitle("Provider");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
---
+++ src/main/java/org/example/course/ProviderController.java
@@ -0,0 +1,25 @@
+package org.example.course;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import org.example.course.entities.Provider;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class ProviderController {
+
+
+
+
+
+
+}
