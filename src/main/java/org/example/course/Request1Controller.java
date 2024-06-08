b3d717277310eeba568cc1dadbf8d65ce18925b3
1.kla$ <recum12233@mail.ru>
2024-06-08 17:46:29 +0300

    sass

--- src/main/java/org/example/course/Request1Controller.java
+++ src/main/java/org/example/course/Request1Controller.java
@@ -1,6 +1,9 @@
 package org.example.course;
 
 import jakarta.persistence.Query;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -9,6 +12,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import org.example.course.entities.EndProduct;
 import org.example.course.entities.Provider;
+import org.hibernate.SessionFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,7 +23,7 @@ public class Request1Controller implements Initializable {
     @FXML
     private Button search_btn;
     @FXML
-    private TableView<EndProduct> request1_table= new TableView<>();
+    private TableView<EndProduct> request1_table = new TableView<>();
     @FXML
     private TableColumn<EndProduct, Long> id;
     @FXML
@@ -33,8 +37,18 @@ public class Request1Controller implements Initializable {
     @FXML
     private TableColumn<EndProduct, Long> assembly_id;
 
-    protected void request1(){
-
+    protected void request1() {
+        Platform.runLater(() -> {
+            HibernateSession.sessionFactory().inTransaction(session -> {
+                Query query = session.createQuery("from EndProduct where timeToProduce=:n order by id desc", Provider.class);
+                query.setParameter("n", Long.parseLong(text_tf.getText()));
+                ObservableList<EndProduct> providerObservableList =
+                        FXCollections.observableArrayList(
+                                query.getResultList()
+                        );
+                request1_table.setItems(providerObservableList);
+            });
+        });
 //        Query query = session.createQuery("from EndProduct where timeToProduce=:n order by id desc", Provider.class);
 //        query.setParameter("n", 2);
 //        ObservableList<Provider> providerObservableList =
@@ -43,6 +57,7 @@ public class Request1Controller implements Initializable {
 //                );
 
     }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         request1();
