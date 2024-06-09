7bb55a0f5166708de81a4723e4702c81d2811cf6
1.kla$ <recum12233@mail.ru>
2024-06-09 21:59:02 +0300

    работает криво, но работает

--- src/main/java/org/example/course/Request3.java
+++ src/main/java/org/example/course/Request3.java
@@ -1,5 +1,9 @@
 package org.example.course;
 
+import jakarta.persistence.Query;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,15 +13,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import org.example.course.entities.CheckSaleComponent;
 import org.example.course.entities.CheckSaleEndProduct;
 import org.example.course.entities.Components;
 import org.example.course.entities.EndProduct;
+import org.example.course.searchRequest.Request2;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Request3 implements Initializable {
@@ -26,11 +34,15 @@ public class Request3 implements Initializable {
     @FXML
     private Button back;
     @FXML
-    private TableView<Components> request3_table = new TableView<Components>();
+    private TableView<Result> request3_table = new TableView<>();
     @FXML
-    private TableColumn<Components, Long> articul;
+    private TableColumn<Result, Long> articul;
     @FXML
-    private TableColumn<CheckSaleComponent, String> scope_of_supply;
+    private TableColumn<Result, Long> scope_of_supply;
+    @FXML
+    private TextField scope;
+    @FXML
+    private TextField time;
     @FXML
     void goBack(ActionEvent event) throws IOException { // GO BACK!!!
         Stage stage = (Stage) back.getScene().getWindow();
@@ -41,12 +53,62 @@ public class Request3 implements Initializable {
         stage.setScene(new Scene(root));
         stage.show();
     }
-
+    @FXML
+    private void search() {
+        Platform.runLater(() -> {
+            HibernateSession.sessionFactory().inTransaction(session -> {
+                Query query = session.createQuery("SELECT c.articul, cs.scopeOfSupply " +
+                        "FROM Components c JOIN CheckSaleComponent cs " +
+                        "ON c.articul = cs.articul " +
+                        "WHERE c.name = :n AND cs.scopeOfSupply = :b");
+                query.setParameter("n", time.getText());
+                query.setParameter("b", Long.parseLong(scope.getText()));
+                List<Result> list = new ArrayList<>(5);
+                for (Object o : query.getResultList()) {
+                    Object[] row = (Object[]) o;
+                    list.add(new Result((Long) row[0], (Long) row[1]));
+                    System.out.println(row[0] + " " + row[1]);
+                }
+                ObservableList<Result> providerObservableList =
+                        FXCollections.observableArrayList(
+                                list
+                        );
+                System.out.println(providerObservableList);
+                request3_table.setItems(providerObservableList);
+                System.out.println(query.getResultList());
+            });
+        });
+    }
     public void request3(ActionEvent event) {
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         articul.setCellValueFactory(new PropertyValueFactory<>("articul"));
         scope_of_supply.setCellValueFactory(new PropertyValueFactory<>("scope_of_supply"));
+    }
+    protected class Result {
+        private long id;
+        private long scope_of_supply;
+
+        public Result(long id, long scope_of_supply) {
+            this.id = id;
+            this.scope_of_supply = scope_of_supply;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public long getScope_of_supply() {
+            return scope_of_supply;
+        }
+
+        public void setScope_of_supply(long scope_of_supply) {
+            this.scope_of_supply = scope_of_supply;
+        }
     }
 }
